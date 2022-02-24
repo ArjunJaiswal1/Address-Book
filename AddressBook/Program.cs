9f559882b8d3c6b0e1f78884ecdc99986e57f5dc
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-24 11:43:37 +0530

    [Arjun] Add ; Added Switched

---
+++ AddressBook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBook
+{
+    public class Program
+    {
+        //Main  Method / program Entry Point
+        static void Main(string[] args)
+        {
+            //Showing Welcome Message On console.
+            Console.WriteLine("==========Welcome To Addess Book Programs==========");
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            AddressBookEntry addressBook = new AddressBookEntry()  //Initializing elements using collection-initializer syntax
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.displayContact();
+            Console.ReadLine();
+        }
+    }
+}
